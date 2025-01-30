485a17b81789092b07fa524aaf6c1982ab8c53ec
Olushola <ememobongumoren2@gmail.com>
2025-01-31 00:17:06 +0100

    update on destination page

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import TitleNav from "@/components/TitleNav";
 import Image from "next/image";
-import React from "react";
-import moonImg from "../../../public/assets/destination/image-moon.png";
+import React, { useState } from "react";
 import { Col, Row } from "antd";
+import data from "@/app/data.json";
 
-const page = () => {
+const Destination = () => {
+  const destinationData = data.destinations
+  const [destinationIndex, setDestinationIndex] = useState(0);
+
+  const handleDestinationClick = (index: number) => {
+    setDestinationIndex(index);
+  };
+
   return (
     <div className="bg_destination">
       <div className="px-28">
@@ -15,32 +24,52 @@ const page = () => {
                 <TitleNav title="Pick your destination" num="01" />
               </Col>
               <Col xs={12}>
-                <div className=" ">
-                  <Image src={moonImg} className="w-full" alt="moon img" />
+                <div className=" w-full ">
+                  <Image
+                    src={destinationData[destinationIndex].images.png}
+                    className="w-full"
+                    alt="moon img"
+                    objectFit="fit"
+                  />
                 </div>
               </Col>
               <Col xs={12}>
                 <div className="">
                   <div className="flex font-bellefairReg text-base text-blue300 uppercase items-center gap-10">
-                    <span>Moon</span>
-                    <span>Mars</span>
-                    <span>Europe</span>
-                    <span>Titan</span>
+                    {destinationData.map((place, index) => (
+                      <span
+                        key={index}
+                        onClick={() => handleDestinationClick(index)}
+                        className="cursor-pointer"
+                      >
+                        {place.name}
+                      </span>
+                    ))}
                   </div>
                   <div className="mt-20 flex flex-col gap-y-10">
-                    <h2 className="text-8xl font-bellefairReg ">MOON</h2>
+                    <h2 className="text-8xl font-bellefairReg ">
+                      {destinationData[destinationIndex].name}
+                    </h2>
                     <p className="text-xl font-barlowCondensedLight text-blue300 ">
-                    See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+                      {destinationData[destinationIndex].description}
                     </p>
                     <hr />
                     <div className="flex gap-20 items-center font-barlowCondensedLight">
                       <div>
-                        <p className="text-blue300 font-barlowCondensedLight">AVG. DISTANCE</p>
-                        <p className="font-bellefairReg uppercase text-xl tracking-widest">384,400 KM</p>
+                        <p className="text-blue300 font-barlowCondensedLight">
+                          AVG. DISTANCE
+                        </p>
+                        <p className="font-bellefairReg uppercase text-xl tracking-widest">
+                          {destinationData[destinationIndex].distance}
+                        </p>
                       </div>
                       <div>
-                        <p className="text-blue300 font-barlowCondensedLight">EST. TRAVEL TIME</p>
-                        <p className="font-bellefairReg uppercase text-xl tracking-widest">3 Days</p>
+                        <p className="text-blue300 font-barlowCondensedLight">
+                          EST. TRAVEL TIME
+                        </p>
+                        <p className="font-bellefairReg uppercase text-xl tracking-widest">
+                          {destinationData[destinationIndex].travel}
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -54,4 +83,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Destination;
